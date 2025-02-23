79a9175d37b6ad105953aa001ef08c28f1b63a7e
DecioFrancis <francisdasilva@gmail.com>
2025-02-23 00:36:49 -0300

    feat: add configuration in cmakelists to run libraries

--- EmbarcatechFinalProject.c
+++ EmbarcatechFinalProject.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
-
-
+#include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "hardware/irq.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
